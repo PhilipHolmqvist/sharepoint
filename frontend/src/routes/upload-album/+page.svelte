3ee9b3f1433793745b30b3abcae368d7e8ae3274
Philip Holmqvist <philip_holmqvist@hotmail.com>
2024-02-11 16:50:16 +0100

    upload album - mobile first

--- frontend/src/routes/upload-album/+page.svelte
+++ frontend/src/routes/upload-album/+page.svelte
@@ -4,98 +4,101 @@
 	<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
 </svelte:head>
 
-
 <script lang="ts">
-	import albumPic from '$lib/images/profile-picture.jpg';
+	import albumPic from '$lib/images/defualt-album.jpg';
 </script>
-
-<style>
-	.bd-placeholder-img {
-	  font-size: 1.125rem;
-	  text-anchor: middle;
-	  -webkit-user-select: none;
-	  -moz-user-select: none;
-	  user-select: none;
-	}
-
-	@media (min-width: 768px) {
-	  .bd-placeholder-img-lg {
-		font-size: 3.5rem;
-	  }
-	}
-  </style>
-
-
-<p>Upload images here</p>
-<input class="form-control" type="file" id="formFileMultiple" multiple />
 
 <div class="container">
 	<main>
-		  <h4 class="mb-3">Upload album</h4>
+	  <div class="row g-5">
+		<div class="col-md-5 col-lg-4 order-md-last">
+		  <h4 class="d-flex justify-content-between align-items-center mb-3">
+			<span class="text-primary">Your album picture</span>
+		  </h4>
+		  
+		  <div class="mx-auto" style="width: 140px;">
+			<div class="d-flex justify-content-center align-items-center rounded" style="height: 140px; background-color: rgb(233, 236, 239);">
+				<img src={albumPic} class="img-thumbnail" alt="">
+			</div>
+		  </div>
+
+  
+		  <form class="card p-2">
+			  <button type="submit" class="btn btn-secondary">Upload album cover</button>
+			  <div class="mb-3">
+				<label for="formFile" class="form-label">Default file input example</label>
+				<input class="form-control" type="file" id="formFile">
+			  </div>
+		  </form>
+		</div>
+
+		<div class="col-md-7 col-lg-8">
+		  <h4 class="mb-3">Create new album</h4>
 		  <form class="needs-validation" novalidate>
 			<div class="row g-3">
 			  <div class="col-sm-6">
-				<label for="firstName" class="form-label">Album name</label>
-				<input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+				<label for="albumName" class="form-label">Album name</label>
+				<input type="text" class="form-control" id="albumName" placeholder="" value="" required>
 				<div class="invalid-feedback">
-				  Valid first name is required.
-				</div>
-			  </div>
-			
-			  <div class="col-12">
-				<div class="form-group">
-					<label for="exampleFormControlTextarea1">Album description</label>
-					<textarea class="form-control" id="album-description" placeholder="This was a fun vacation in 2019..." rows="3"></textarea>
-				  </div>
-			  </div>
-
-			  <div class="col-6">
-				<div class="mx-auto" style="width: 140px;">
-				  <div class="d-flex justify-content-center align-items-center rounded" style="height: 140px; background-color: rgb(233, 236, 239);">
-					  <img src={albumPic} class="img-thumbnail" alt="">
-				  </div>
-				</div>
-			  </div>
-  
-			  <div class="col-12">
-				<label for="username" class="form-label">Username</label>
-				<div class="input-group has-validation">
-				  <span class="input-group-text">@</span>
-				  <input type="text" class="form-control" id="username" placeholder="Username" required>
-				<div class="invalid-feedback">
-					Your username is required.
-				  </div>
+				  Valid album name is required.
 				</div>
 			  </div>
 
-			<hr class="my-4">  
-  
-			  <div class="col-12">
+			  <div class="col-sm-6">
+			  <div class="form-group">
+				<label for="albumDescription">Description</label>
+				<textarea class="form-control" id="albumDescription" rows="3"></textarea>
+			  </div>
+			</div>
+			  <div class="col-md-5">
 				<label for="country" class="form-label">Country</label>
 				<select class="form-select" id="country" required>
-				  <option value="">Choose...</option>
+				  <option value="">Choose...</option> <!-- Add more options-->
 				  <option>United States</option>
+				  <option>Sweden</option>
 				</select>
 				<div class="invalid-feedback">
 				  Please select a valid country.
 				</div>
 			  </div>
+			  <div class="col-md-5">
+			  <p>Upload images here</p>
+				<input class="form-control" type="file" id="formFileMultiple" multiple />
+			</div>
+			</div>
 
 			<hr class="my-4">
   
 			<div class="form-check">
+			  <input type="checkbox" class="form-check-input" id="private-album">
+			  <label class="form-check-label" for="private-album">Make my album private</label>
+			</div>
+  
+			<div class="form-check">
 			  <input type="checkbox" class="form-check-input" id="save-info">
-			  <label class="form-check-label" for="save-info">Make album private</label>
+			  <label class="form-check-label" for="save-info">This button doesnt do anything</label>
 			</div>
   
 			<hr class="my-4">
   
-			
+			<h4 class="mb-3">Add colaborators</h4>
   
+			<div class="row gy-3">
+			  <div class="col-md-6">
+			  <div class="input-group mb-3">
+				<input type="text" class="form-control" placeholder="Colaborator's email" aria-label="Recipient's username" aria-describedby="basic-addon2">
+				<div class="input-group-append">
+				  <button class="btn btn-outline-secondary" type="button" disabled>Add</button>
+				</div>
+			  </div>
+			</div>
 			<hr class="my-4">
-  
 			<button class="w-100 btn btn-primary btn-lg" type="submit">Create album</button>
 		  </form>
+		</div>
+	  </div>
 	</main>
+
+	
 </div>
 
